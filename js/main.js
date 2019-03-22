8a877656eff434466140d9d70f47104cf0d5ff60
PsykeSPB <psykespb@gmail.com>
2019-03-22 15:24:23 +0300

    modular template

--- js/main.js
+++ js/main.js
@@ -1,23 +1,5 @@
-function draw() {
-  // Prepare global variables
-  const body = document.querySelector('body');
-  const screen = {
-    width: body.getBoundingClientRect().width,
-    height: body.getBoundingClientRect().height,
-  };
-  // Prepare canvas
-  const canvas = document.createElement('canvas');
-  canvas.id = 'canvas';
-  canvas.width = screen.width;
-  canvas.height = screen.height;
-  body.appendChild(canvas);
-  // Try to initiate canvas
-  if (canvas.getContext) {
-    const ctx = canvas.getContext('2d');
-    // cover background in black
-    ctx.fillStyle = 'rgb(0, 0, 0)';
-    ctx.fillRect(0, 0, screen.width, screen.height);
-  } else {
-    console.error('canvas cant be initiated');
-  }
+import { Plotter } from './plotter';
+
+function init() {
+  const plotter = new Plotter();
 };
---
+++ js/plotter.js
@@ -0,0 +1,5 @@
+export class Plotter {
+    constructor() {
+        console.log('plotter initiated');
+    };
+};
